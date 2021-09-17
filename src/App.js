43cddfc15cdeb7021315b6accb766af20475a9ae
Eugene Geller <EGeller@chainbridgetech.com>
2021-09-16 21:29:57 -0500

    After Meet another React Component

--- src/App.js
+++ src/App.js
@@ -18,30 +18,42 @@ const list = [
   },
 ];
 
+function List() {
+  return (
+    <ul>
+      {list.map(function (item) {
+        return (
+          <li key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
+
+function Search() {
+  return (
+    <div>
+      <label htmlFor="search">Search:</label>
+      <input id="search" type="text" />
+    </div>
+  );
+}
+
 function App() {
   const title = "React";
   return (
     <div>
       <h1>My hacker stories</h1>
-
-      <label htmlFor="search">Search:</label>
-      <input id="search" type="text" />
-
+      <Search />
       <hr />
-      <ul>
-        {list.map(function (item) {
-          return (
-            <li key={item.objectID}>
-              <span>
-                <a href={item.url}>{item.title}</a>
-              </span>
-              <span>{item.author}</span>
-              <span>{item.num_comments}</span>
-              <span>{item.points}</span>
-            </li>
-          );
-        })}
-      </ul>
+      <List />
     </div>
   );
 }
