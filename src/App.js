4719da7f2cfc0d76715cd0f25a792913cea02fc9
Eugene Geller <EGeller@chainbridgetech.com>
2021-09-17 15:34:59 -0500

    after Handler Functions

--- src/App.js
+++ src/App.js
@@ -34,10 +34,14 @@ const List = () => (
 );
 
 const Search = () => {
+  const handleChange = (event) => {
+    console.log(event.target.value);
+  };
+
   return (
     <div>
       <label htmlFor="search">Search:</label>
-      <input id="search" type="text" />
+      <input id="search" type="text" onChange={handleChange} />
     </div>
   );
 };
