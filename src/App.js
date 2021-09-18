eb1b7adf78576c9164b56602aec8efd21ae235c4
Eugene Geller <EGeller@chainbridgetech.com>
2021-09-17 22:22:32 -0500

    Making it look like in the book

--- src/App.js
+++ src/App.js
@@ -1,5 +1,55 @@
 import * as React from "react";
 
+const App = () => {
+  const stories = [
+    {
+      title: "React",
+      url: "https://reactjs.org/",
+      author: "Jordan Walke",
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: "Redux",
+      url: "https://redux.js.org/",
+      author: "Dan Abramov, Andrew Clark",
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
+
+  const [searchTerm, setSearchTerm] = React.useState("React");
+
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const searchedStories = stories.filter((story) =>
+    story.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  return (
+    <div>
+      <h1>My Hacker Stories</h1>
+
+      <Search search={searchTerm} onSearch={handleSearch} />
+
+      <hr />
+
+      <List list={searchedStories} />
+    </div>
+  );
+};
+
+const Search = ({ search, onSearch }) => (
+  <div>
+    <label htmlFor="search">Search: </label>
+    <input id="search" type="text" value={search} onChange={onSearch} />
+  </div>
+);
+
 const List = ({ list }) => (
   <ul>
     {list.map((item) => (
@@ -18,56 +68,5 @@ const Item = ({ item }) => (
     <span>{item.points}</span>
   </li>
 );
-
-const Search = ({ search, onSearch }) => (
-  <div>
-    <label htmlFor="search">Search:</label>
-    <input id="search" type="text" value={search} onChange={onSearch} />
-  </div>
-);
-
-const App = () => {
-  console.log("App renders...");
-  const stories = [
-    {
-      title: "React",
-      url: "https://reactjs.org",
-      author: "Jordan Walke",
-      num_comments: 3,
-      points: 4,
-      objectID: 0,
-    },
-    {
-      title: "Redux",
-      url: "https://redux.js.org/",
-      author: "Dan Abramov, Andrew Clark",
-      num_comments: 2,
-      points: 5,
-      objectID: 1,
-    },
-  ];
-
-  const [searchTerm, setSearchTerm] = React.useState("React");
-  const handleSearch = (event) => {
-    setSearchTerm(event.target.value);
-  };
-
-  const searchedStories = stories.filter((story) =>
-    story.title.toLowerCase().includes(searchTerm.toLowerCase())
-  );
-
-  // const searchedStories = stories.filter(function (story) {
-  //   return story.title.includes(searchTerm);
-  // });
-
-  return (
-    <div>
-      <h1>My hacker stories</h1>
-      <Search search={searchTerm} onSearch={handleSearch} />
-      <hr />
-      <List list={searchedStories} />
-    </div>
-  );
-};
 
 export default App;
