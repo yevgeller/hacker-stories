6980862426727271025c380d49ffa98056758aa7
Eugene Geller <EGeller@chainbridgetech.com>
2021-09-21 22:47:12 -0500

    After Memoized Handlers

--- src/App.js
+++ src/App.js
@@ -55,7 +55,7 @@ const App = () => {
     isError: false,
   });
 
-  React.useEffect(() => {
+  const handleFetchStories = React.useCallback(() => {
     if (!searchTerm) return;
 
     dispatchStories({ type: "STORIES_FETCH_INIT" });
@@ -70,6 +70,10 @@ const App = () => {
       })
       .catch(() => dispatchStories({ type: "STORIES_FETCH_FAILURE" }));
   }, [searchTerm]);
+
+  React.useEffect(() => {
+    handleFetchStories();
+  }, [handleFetchStories]);
 
   const handleRemoveStory = (item) => {
     dispatchStories({
